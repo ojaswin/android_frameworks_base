3c98e65eaa9fe02c42ffd2e970d9cd93230a7b40
ojaswin <ojaswinsinghjosan@gmail.com>
2016-09-09 18:35:47 +0530

    Disable/Enable screenshot sound [1/2] Credits: htchoi1995(https://github.com/htchoi1995) Original Commit: https://github.com/Cardinal-AOSP/frameworks_base/commit/baa7f611076e12611cbf5ebd6ce0484b741626d1
    Change-Id: I03057eeffa76aecc063f35b1566308e165fb3d57

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3571,6 +3571,13 @@ public final class Settings {
          * @hide
          */
         public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+
+        /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
  
 
         /**
